c54703e7137e2669c34afd75f9b70b19d9bf7922
Alper Kılıçkaya <48261006+alperkilickaya@users.noreply.github.com>
2026-02-20 01:32:26 +0100

    some changes

--- services/services.js
+++ services/services.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiUrl = 'https://api.themoviedb.org/3';
-const apiKey = 'api_key=29f6570f2c4242fab2679765257fc6e3';
+const apiKey = `api_key=${process.env.EXPO_PUBLIC_TMDB_API_KEY}`;
 
 export const getPopularMovies = async () => {
   const resp = await axios.get(`${apiUrl}/movie/popular?${apiKey}`);
